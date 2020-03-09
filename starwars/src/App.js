e1c40bdef7a347ae59b05ac44ea53ac9e1d722bd
jaren <jarensmart@yahoo.com>
2020-03-08 21:37:35 -0400

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CharCard from "./CharCard";
 import "./App.css";
+import styled from "styled-components";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
--- starwars/src/CardData.js
+++ starwars/src/CardData.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 const CardBox = styled.div`
   border: 2px solid black;
-  height: 85vh;
-  width: 398px;
+  height: 70vh;
+  width: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -41,14 +41,18 @@ const CharGender = styled.p`
 const CardData = props => {
   return (
     <CardBox>
-      <CharName>Name: {props.data.name}</CharName>
-      <CharBody>
-        Height: {props.data.height}, Mass: {props.data.mass}
-      </CharBody>
-      <CharHair>Hair Color: {props.data.hair_color}</CharHair>
-      <CharSkin>Skin Color: {props.data.skin_color}</CharSkin>
-      <CharBirth>Born: {props.data.birth_year}</CharBirth>
-      <CharGender>Gender: {props.data.gender}</CharGender>
+      <div className="Header">
+        <CharName>Name: {props.data.name}</CharName>
+      </div>
+      <section className="Char-info">
+        <CharBody>
+          Height: {props.data.height}, Mass: {props.data.mass}
+        </CharBody>
+        <CharHair>Hair Color: {props.data.hair_color}</CharHair>
+        <CharSkin>Skin Color: {props.data.skin_color}</CharSkin>
+        <CharBirth>Born: {props.data.birth_year}</CharBirth>
+        <CharGender>Gender: {props.data.gender}</CharGender>
+      </section>
     </CardBox>
   );
 };
